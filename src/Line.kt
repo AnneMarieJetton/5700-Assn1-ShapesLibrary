84057f21c83b43a3121ff21fe7d1f497e1a59c42
Anne Jetton <annemariejetton@gmail.com>
2022-05-14 10:37:33 -0600

    implemented line

--- src/Line.kt
+++ src/Line.kt
@@ -1,15 +1,23 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class Line(points: MutableList<Point>): Shape(points) {
 
-    fun getSlope(){
-
+    fun getSlope(): Double {
+        return (points[1].y - points[0].y) / (points[1].x - points[0].x)
     }
 
-    fun getLength(){
-
+    fun getLength(): Double {
+        return sqrt((points[1].x - points[0].x).pow(2) + (points[1].y - points[0].y).pow(2))
     }
 
-    private fun checkLength() {
+    private fun checkLength(): Boolean {
+        val length = getLength()
+        if (length.toInt() == 0) {
+            return false
+        }
 
+        return true
     }
 
 }
