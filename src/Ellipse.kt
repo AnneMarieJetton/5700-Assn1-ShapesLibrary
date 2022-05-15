8cd529b75b367eb1f696d1d493cb17f2e2f1d995
Anne Jetton <annemariejetton@gmail.com>
2022-05-14 21:11:22 -0600

    fixed variables

--- src/Ellipse.kt
+++ src/Ellipse.kt
@@ -1,6 +1,6 @@
 open class Ellipse(_points: MutableList<Point>, _radii: MutableList<Double>): Shape(_points) {
     var radii: MutableList<Double> = _radii
-    var area = Math.PI * radii[0] * radii[1]
+    var area: Double = Math.PI * radii[0] * radii[1]
 
     private fun checkArea(): Boolean {
         if (area == 0.toDouble()){
--- src/Rectangle.kt
+++ src/Rectangle.kt
@@ -1,8 +1,8 @@
 import kotlin.math.abs
 
 open class Rectangle(_points: MutableList<Point>): Shape(_points) {
-    val width = abs(points[1].x - points[0].x)
-    val height = abs(points[1].y - points[0].y)
+    val width: Double = abs(points[1].x - points[0].x)
+    val height: Double = abs(points[1].y - points[0].y)
 
     fun getArea(): Double {
         return width * height
